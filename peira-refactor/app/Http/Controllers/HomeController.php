afb960b72f57455504e62cf659544847a30dbd72
Alix Baur <hi@blended-shapes.com>
2025-04-22 09:36:13 +0200

    Apply minor layout changes

--- peira-refactor/app/Http/Controllers/HomeController.php
+++ peira-refactor/app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
     public function index(DrupalApiService $drupal, $locale = 'de')
     {
-        app()->setLocale($locale); // sets current app locale
+        app()->setLocale($locale);
 
         $newsRaw = $drupal->getNews();
 
